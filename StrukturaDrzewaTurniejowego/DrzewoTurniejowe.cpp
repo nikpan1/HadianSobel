942b452006b051282398bc6ee6ebb78a94acfdcf
Nikodem <nikodem.panknin@gmail.com>
2022-12-30 22:43:29 +0100

    before new construct alg

--- StrukturaDrzewaTurniejowego/DrzewoTurniejowe.cpp
+++ StrukturaDrzewaTurniejowego/DrzewoTurniejowe.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
-#include <math.h>  // log
-#include <cmath>  // ceil and pow
-#include <random>
+#include <math.h>    // log2
+#include <cmath>     // ceil, pow
+#include <random>    // rand
 
 using namespace std;
 
-#define treeHeight(l) int(ceil(log2(l)))             // wysokość drzewa = sufit(log2(l)), gdzie l - ilość liści
-#define treeSize(h) int(pow(2, h + 1) - 1)           // rozmiar drzewa = 2^(h+1) + 1, gdzie h - wysokość drzewa
-#define firstLevelsId(n) int(pow(2, n))              // pierwszy indeks n-tej warstwy = 2^n
-#define lastLevelsId(n) int(pow(2, n + 1) - 1)       // ostatni indeks n-tej warstwy = 2^(n+1) - 1
-#define parentID(childID) int(floor(childID / 2))    // indeks rodzica = entier(childID / 2), gdzie childID to indeks dziecka
-#define L_ChildID(parentID) int(parentID * 2)        // lewe dziecko = 2 * parentId, gdzie parentID to indeks rodzica  
-#define R_ChildID(parentID) int(2 * parentID + 1)    // prawe dziecko = 2 * parentID + 1, gdzie parentID to indeks rodzica
-#define howMuchNodes(l, h) int(l - pow(2, h - 1))    // ilość węzłów w drzewie = l - 2^(h-1), gdzie l - ilość liści, h - wysokość drzewa 
 
-void Visualize(int (*tree)[2], int treeSize, int* leaves, int leavesSize);
-void BuildNode(int (*tree)[2], int nodeID);
+#define treeHeight(l) int(ceil(log2(l)))              // wysokość drzewa = sufit(log2(l)), gdzie l - ilość liści
+#define treeSize(h) int(pow(2, h + 1) - 1)            // rozmiar drzewa = 2^(h+1) + 1, gdzie h - wysokość drzewa
+#define firstLevelsId(n) int(pow(2, n))               // pierwszy indeks n-tej warstwy = 2^n
+#define lastLevelsId(n) int(pow(2, n + 1) - 1)        // ostatni indeks n-tej warstwy = 2^(n+1) - 1
+#define parentID(childID) int(floor(childID / 2))     // indeks rodzica = entier(childID / 2), gdzie childID to indeks dziecka
+#define L_ChildID(parentID) int(parentID * 2)         // lewe dziecko = 2 * parentId, gdzie parentID to indeks rodzica  
+#define R_ChildID(parentID) int(2 * parentID + 1)     // prawe dziecko = 2 * parentID + 1, gdzie parentID to indeks rodzica
+#define howMuchNodes(l, h) int(l - 1 - pow(2, h - 1)) // ilość węzłów w drzewie = l - 2^(h-1), gdzie l - ilość liści, h - wysokość drzewa 
+
 void GenerateTreeFromArray(int* leaves, int leavesSize);
+void BuildNode(int (*tree)[2], int nodeID);
 void Replacemax(int (*tree)[2], int newMax);
+void Visualize(int (*tree)[2], int treeSize, int* leaves, int leavesSize);
 
 void BuildNode(int (*tree)[2], int nodeID) {
   int LC = L_ChildID(nodeID);
@@ -39,42 +40,49 @@ void BuildNode(int (*tree)[2], int nodeID) {
   }
 }
 
-void GenerateTreeFromArray(int* leaves, int leavesSize) {
-  int height = treeHeight(leavesSize); 
-  int size = treeSize(height);
-  int tree[size + 1][2] = {0}; // indexujemy od 1
-  int n = firstLevelsId(height - 1);
-  int m = lastLevelsId(height - 1);
-  int h = howMuchNodes(leavesSize - 1, height);
+inline void GenerateTreeFromArray(int* leaves, int leavesSize) {
+  int h = treeHeight(leavesSize);   // wysokość drzewa
+  int mem = treeSize(h) + 1;        // liczba pamięci potrzebnej do zaalokowania pamięci 
+  int tree[mem][2] = {0};           // drzewo turniejowe, w postaci dwuwymiarowej tablicy 2 x AM, indexowane od 1
+                                    // (n, m) to przedział przedostatniej warstwy 
+  
+  int n = firstLevelsId(h - 1), m = lastLevelsId(h - 1);
+  int nodes = howMuchNodes(leavesSize, h);    // ilość węzłów w przedostatniej warstwy
   int pivot = 0;
 
-  cout << "height:" << height << endl << "size:" << size << endl;      
-  cout << "przedział p-1 warstwy: " << n << '-' << m << endl;
-  cout << "Ile węzłów w p-1? - " << h << endl;
-
-  for (int i = n; i <= n + h; i ++) {  // tutaj węzły
+  // od n do n + nodes tworzymy węzły, które jako dzieci mają liście
+  for (int i = n; i <= n + nodes; i ++) { 
     tree[L_ChildID(i)][0] = leaves[pivot ++];
     tree[R_ChildID(i)][0] = leaves[pivot ++];    
     BuildNode(tree, i);
   }
   
-  for (int j = n + h + 1; j <= m; j ++) tree[j][0] = leaves[pivot ++];
+  // w pozostałych (od n + nodes + 1 do m) przechowujemy liście
+  for (int j = n + nodes + 1; j <= m; j ++) tree[j][0] = leaves[pivot ++];  
+  // następnie iterujemy od n - 1 do 1 budując kolejne węzły
   for (int i = n - 1; i > 0; i --) BuildNode(tree, i);
-  Visualize(tree, height, leaves, leavesSize);
+
+  Visualize(tree, h, leaves, leavesSize);
 }
 
 void Replacemax(int (*tree)[2], int newMax) {
-  cout << " ";
+  int pivot = tree[1][1];
+  tree[pivot][0] = newMax;
+  
+  while (pivot >= 1) {
+    
+    pivot = parentID(pivot);
+  }
 }
+
 
 int main() {
   srand( time(NULL) );
-  int leaves_size = 4 + rand() % 10;
-  int leaves [leaves_size];
-  cout << "leaves_size = " << leaves_size << endl;
-  for(int i = 0; i < leaves_size; i ++) leaves[i] = 100 + rand() % 100;
-  
-  GenerateTreeFromArray(leaves, leaves_size);
+  int leavesSize = 4 + rand() % 10;
+  int leaves [leavesSize];
+
+  for(int i = 0; i < leavesSize; i ++) leaves[i] = 100 + rand() % 100; 
+  GenerateTreeFromArray(leaves, leavesSize);
   return 0;
 }
 
@@ -85,7 +93,7 @@ void Visualize(int (*tree)[2], int treeSize, int* leaves, int leavesSize) {
   cout << endl << "Tournament tree: " << endl;
   for (int j = 0; j <= treeSize; j ++) {
     for (int i = firstLevelsId(j); i <= lastLevelsId(j); i ++) { 
-      cout << i << "|" << tree[i][0] << ":" << tree[i][1] << '\t';
+      cout << i << "|" << tree[i][0] << ":" << tree[i][1] << "\t\t";
     }
     cout << endl;
   }
