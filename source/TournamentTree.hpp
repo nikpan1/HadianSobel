9082bf8d2bdfba58ecf443bf857f40a0abd146d4
nikpan1 <96376932+nikpan1@users.noreply.github.com>
2023-01-17 12:47:48 +0100

    actually, proper complexity

--- source/TournamentTree.hpp
+++ source/TournamentTree.hpp
@@ -21,9 +21,9 @@ using namespace std;
 
 struct node { int mx; int mn; };     // struktura węzłów drzewa
 
-vector<node> Construct(array* leaves, double& counter); // funkcja budująca strukturę drzewa turniejowego
+vector<node> Construct(array* leaves); // funkcja budująca strukturę drzewa turniejowego
 void UpdatePath(vector<node>& tree, int pivot);         // funkcja aktualizująca drzewo przechodząc od liścia wskazywanego przez pivot, aż do korzenia 
-void Replacemax(vector<node>& tree, int newVal);        // funkcja wstawiająca nową wartość na miejsce starego maximum drzewa turniejowego
+void Replacemax(vector<node>& tree, int newVal, double &counter);        // funkcja wstawiająca nową wartość na miejsce starego maximum drzewa turniejowego
 void HadianSobel(array* list, int k);                   // funkcja wykonująca algorytm hadiana-Sobela wyznaczając k największych wartości
 
 
@@ -62,13 +62,14 @@ vector<node> Construct(array* leaves) {
   return tree;
 }
 
-void Replacemax(vector<node>& tree, int newVal) {
+void Replacemax(vector<node>& tree, int newVal, double& counter) {
   // nowa wartość na pozycji starego maximum drzewa
   val(1) = newVal; 
  
   // schodzenie od liścia, gdzie znajduje się nowa wartość, aż do korzenia
   int pivot = twigID(tree[1].mx);
   while (pivot > 1) {
+    counter ++;
     pivot = parentID(pivot);
     // aktualizuje przy każdej iteracji węzeł wskazywany przez pivot
     UpdatePath(tree, pivot);
@@ -76,20 +77,21 @@ void Replacemax(vector<node>& tree, int newVal) {
 }
 
 void HadianSobel(array* list, int k) {
+  double counter = list->size - 1;
   // rozmiar listy wejściowej
   int n = list->size; 
   // wydzielenie tablicy o rozmiarze n - k + 1
   array leaves = {list->l, short(n - k + 1)};
   // tworzenie drzewa
   vector<node> tree = Construct(&leaves);
-  
+
   // czytanie oraz wywołanie Replacemax n-k+1 razy 
   for (int i = n - k + 1; i < n; i ++) {
-    cout << val(1) << SEPARATOR;
-    Replacemax(tree, leaves.l[n-k + i]);
+    //cout << val(1) << SEPARATOR;
+    Replacemax(tree, leaves.l[n-k + i], counter);
   }
-
-  cout << val(1) << endl;
+  cout << counter <<endl;
+  //cout << val(1) << endl;
 }
 
 // _______________________
